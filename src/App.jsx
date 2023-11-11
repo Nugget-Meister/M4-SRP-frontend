434becae1c7ab7a74911be27ade9f280d781ad68
Nugget-Meister <tremainecamacho@gmail.com>
2023-11-10 23:26:15 -0500

    Complete placeholder routes w/ Item boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import NavBar from './common/Navbar.jsx'
 import './App.css'
 import Home from './Home/Home.jsx'
+import ItemDetails from './ItemDetails/ItemDetails.jsx'
+import EditItem from './Item/EditItem.jsx'
+import NewItem from './Item/NewItem.jsx'
 
 
 function App() {
@@ -14,6 +17,9 @@ function App() {
         <main>
           <Routes>
             <Route path='/' element={<Home/>}/>
+            <Route path='/:id' element={<ItemDetails/>}/>
+            <Route path='/:id/edit' element={<EditItem/>}/>
+            <Route path='/new' element={<NewItem/>}/>
           </Routes>
         </main>
       </Router>
