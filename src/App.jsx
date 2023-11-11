864b10e81f3cdf39bf9e43d44aa15816ea44cda0
Nugget-Meister <tremainecamacho@gmail.com>
2023-11-11 02:29:24 -0500

    navbar now provides proper navigation, added bootstrap for missing className styles

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ function App() {
 
   return (
     <>
-      <NavBar/>
       <Router>
+        <NavBar/>
         <main>
           <Routes>
             <Route path='/' element={<Home/>}/>
--- src/common/NavBar.jsx
+++ src/common/NavBar.jsx
@@ -3,15 +3,26 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
 const NavBar = () => {
     return (
-        <div>
+        <div className='NavBar'>
             <Navbar expand="lg" className='bg-body-tertiary'>
                 <Container>
-                    <Nav.Link>Home</Nav.Link>
-                    <Nav.Link>New Shelf</Nav.Link>
+                    <Nav.Link href="/" className='px-4'>
+                        Home
+                    </Nav.Link>
+                    <Nav.Link href="/new" classname='px-4'>
+                        New Entry
+                    </Nav.Link>
+                </Container>
+                <Container className='justify-content-center'>
                     <Navbar.Brand>Shelf Space</Navbar.Brand>
-                    <Nav.Link>Log-in / Sign Up</Nav.Link>
+                </Container>
+                <Container className='justify-content-end'>
+                    <Nav.Link href='/login' className='mx-4'>
+                        Log-in / Sign Up
+                    </Nav.Link>
                 </Container>
             </Navbar>
         </div>
