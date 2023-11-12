7efea07270ca582b052eee03abeb23ed13377c1e
Nugget-Meister <tremainecamacho@gmail.com>
2023-11-12 17:48:22 -0500

    completion of getAllItems

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getAllItems } from '../common/helpers/apicalls';
 
 const Home = () => {
+
+    useEffect(() => {
+        getAllItems()
+        .then(res => console.log(res))
+    }, [])
     return (
         <div>
             Home Page
--- src/common/helpers/apicalls.js
+++ src/common/helpers/apicalls.js
@@ -1,8 +1,25 @@
 
-const UTL = import.meta.env.VITE_API_URL
+const URL = import.meta.env.VITE_API_URL
 
-const getAllItems = () => {}
-const getItem = () => {}
-const addItem = () => {}
-const updateItem = () => {}
-const deleteItem = () => {}
+
+
+const getAllItems = () => {
+    return fetch(URL)
+    .then(res => res.json())
+    .then(json => json.data.payload)
+    .catch(err => console.error(err))
+}
+const getItem = (id) => {
+
+}
+const addItem = (item) => {}
+const updateItem = (id, item) => {}
+const deleteItem = (id) => {}
+
+export  {
+    getAllItems,
+    getItem,
+    updateItem,
+    addItem,
+    deleteItem
+}
