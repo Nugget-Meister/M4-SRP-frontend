1bad9adb9c7fda2d0e6f343ae736af4c80f5547d
Nugget-Meister <tremainecamacho@gmail.com>
2023-11-12 15:21:35 -0500

    init apicalls.js, boilerplate for routes

---
+++ src/common/helpers/apicalls.js
@@ -0,0 +1,8 @@
+
+const UTL = import.meta.env.VITE_API_URL
+
+const getAllItems = () => {}
+const getItem = () => {}
+const addItem = () => {}
+const updateItem = () => {}
+const deleteItem = () => {}
