9f64427505f45ae78ee2c801b62764ce1637bbaa
Nugget-Meister <tremainecamacho@gmail.com>
2023-11-14 16:16:48 -0500

    fixed navigation target for submission. item form now allows for use in create & editing data

--- src/Item/EditItem.jsx
+++ src/Item/EditItem.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import ItemForm from './ItemForm';
+import { getItem } from '../common/helpers/apicalls';
 
 const EditItem = () => {
     const {id} = useParams()
+    const [data, setData] = useState({})
 
+    useEffect(() => {
+        getItem(id)
+        .then(res => setData(res))
+    },[])
+
+    // console.log(data)
     return (
         <div>
-            EditItem {id}
+            <h1>Edit Item {id}</h1>
+            <ItemForm data={data}/>
         </div>
     );
 }
