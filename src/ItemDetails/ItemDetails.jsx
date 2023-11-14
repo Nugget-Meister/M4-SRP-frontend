6e227895763cfecf2c909b8ab02150530db0d6b5
elisaul batista <elisaulbatista@pursuit.org>
2023-11-13 19:30:50 -0500

    make the itemdetails works

--- src/ItemDetails/ItemDetails.jsx
+++ src/ItemDetails/ItemDetails.jsx
@@ -1,31 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
-import { getAllItems } from "../common/helpers/apicalls";
+import { getItem } from "../common/helpers/apicalls";
 
 const ItemDetails = () => {
   const { id } = useParams();
   const [book, setBook] = useState(null);
 
-  const Url = getAllItems();
-
   useEffect(() => {
-    const fetchBookDetails = async () => {
-      try {
-        const response = await fetch(`${Url}/books/${id}`);
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        setBook(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchBookDetails();
-  }, [id, Url]);
+    getItem(id).then((res) => setBook(res));
+  });
 
   if (!book) {
     return <div>Loading....</div>;
@@ -33,13 +16,13 @@ const ItemDetails = () => {
 
   return (
     <div className="container">
-      <h1>Title: {book.title}</h1>
-      <p>ISBN: {book.ISBN}</p>
-      <p>Author: {book.author}</p>
-      <p>Genre: {book.genre}</p>
-      <p>Book Rating: {book.bookRating}</p>
-      <p>Has Read: {book.hasRead ? "Yes" : "No"}</p>
-      <img src={book.imageURL} alt={book.title} className="img-fluid" />
+      <h1> {book.title}</h1>
+      <p> {book.isbn}</p>
+      <p> {book.author}</p>
+      <p> {book.genre}</p>
+      <p> {book.bookrating}</p>
+      <p>Has Read : {book.hasRead ? "Yes" : "No"}</p>
+      <img src={book.imageurl} alt={book.title} className="img-fluid" />
     </div>
   );
 };
--- src/common/helpers/apicalls.js
+++ src/common/helpers/apicalls.js
@@ -1,61 +1,48 @@
-
-const URL = import.meta.env.VITE_API_URL
-
-
+const URL = import.meta.env.VITE_API_URL;
 
 const getAllItems = () => {
-    return fetch(URL)
-    .then(res => res.json())
-    .then(json => json.data.payload)
-    .catch(err => console.error(err))
-}
+  return fetch(URL)
+    .then((res) => res.json())
+    .then((json) => json.data.payload)
+    .catch((err) => console.error(err));
+};
 const getItem = (id) => {
-    return fetch(`${URL}/${id}`)
-    .then(res => res.json())
-    // .then(json => json.data.payload)
-    .catch(err => console.error(err))
-}
-const addItem = (data) => {
-    const options = {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {"Content-Type": "application/json"}
-    }
-    return fetch(URL, options)
-    .then(res => res.json())
-    .catch(err => console.error(err))
+  return fetch(`${URL}/${id}`)
+    .then((res) => res.json())
 
-}
+    .catch((err) => console.error(err));
+};
+const addItem = (data) => {
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: { "Content-Type": "application/json" },
+  };
+  return fetch(URL, options)
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+};
 
 const updateItem = (id, data) => {
-    const options = {
-        method: "PUT",
-        body: JSON.stringify(data),
-        headers: {"Content-Type": "application/json"}
-    }
+  const options = {
+    method: "PUT",
+    body: JSON.stringify(data),
+    headers: { "Content-Type": "application/json" },
+  };
 
-    return fetch(`${URL}/${id}`, options)
-    // .then(res => res.json())
-    // .catch(err => console.error(err))
-}
+  return fetch(`${URL}/${id}`, options);
+};
 const deleteItem = (id) => {
-    const options = {
-        method: "delete",
-    }
+  const options = {
+    method: "delete",
+  };
 
-    return fetch(`${URL}/${id}`, options)
-    .then(res => res.json())
-    .then(json => {
-        // Success check
-        return json.payload.data.id ? true : false
+  return fetch(`${URL}/${id}`, options)
+    .then((res) => res.json())
+    .then((json) => {
+      return json.payload.data.id ? true : false;
     })
-    .catch(err => console.error(err))
-}
+    .catch((err) => console.error(err));
+};
 
-export  {
-    getAllItems,
-    getItem,
-    updateItem,
-    addItem,
-    deleteItem
-}
+export { getAllItems, getItem, updateItem, addItem, deleteItem };
