378dfe6d9349058653df343b1a264d44a1da29ca
Nugget-Meister <tremainecamacho@gmail.com>
2023-11-13 19:38:44 -0500

    item now nagigate to itemdetails when clicked, added overflow on images to fit more with initial designs

--- src/Home/Views/ListEntry.jsx
+++ src/Home/Views/ListEntry.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import  Image  from 'react-bootstrap/Image';
 import  Card  from 'react-bootstrap/Card';
+import { useNavigate } from 'react-router';
 
 const ListEntry = ({entry}) => {
     // console.log(entry)
+
+const navigate = useNavigate()
+
+
     return (
-        <tr className=''>
-            <td width="100px" className=''>
-                     <Card variant="top" className="p-0" 
+        <tr 
+            className=''
+            onClick={() => navigate(`/${entry.id}`)}
+            >
+            <td width="10rem" className=''>
+                     {/* <Card variant="top" className="p-0" 
                      style={{width: "10rem", height:"5rem",overflow: "hidden"}}>
                         <Card.Img src={entry.imageurl}/>
-                     </Card>
+                     </Card> */}
             </td>
             <td>{entry.title}</td>
             <td className=''>{entry.hasread ? "Read" : null}</td>
