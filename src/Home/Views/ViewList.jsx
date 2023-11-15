20b295a5fc0085905c55f9b503d8d550bd99de29
Nugget-Meister <tremainecamacho@gmail.com>
2023-11-15 04:35:47 -0500

    migrate listentry from table to divs for react bootstrap, navbar properly scales to entire screen

--- src/Home/Views/ViewList.jsx
+++ src/Home/Views/ViewList.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import ListEntry from './ListEntry';
 import Table from 'react-bootstrap/Table';
+import { Container } from 'react-bootstrap';
 
 const ViewList = ({ entries, onItemClick }) => {
     entries = entries || [];
 
     return (
-        <div className='ViewList'>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Title</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
+        <div className='ViewList m-3'>
+            <div>
+                <h1 className='font-imfellfc'>Current Shelf</h1>
+            </div>
+            <Container
+                className=''
+            >
                     {entries.length > 0
                         ? entries.map((entry) => (
                               <ListEntry
@@ -25,8 +23,7 @@ const ViewList = ({ entries, onItemClick }) => {
                               />
                           ))
                         : null}
-                </tbody>
-            </Table>
+            </Container>
         </div>
     );
 };
--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Image from 'react-bootstrap/Image';
+import loginImage from "../assets/loginImage.jpg"
 
 const Login = () => {
     return (
         <div>
-            Login Page
+
+            <Container className='widget-entry rounded my-4 d-flex'>
+                <Container className='flex-fill'>Welcome!</Container>
+                <Container className='flex-fill'>
+                    <Image 
+                        src={loginImage}
+                        className='img-fluid'
+                        />
+                </Container>
+            </Container>
         </div>
     );
 }
